9a3a240776a0c7749b220f0aee1ac0b416860577
Ravi Kumar Gupta <ravikgupta32@gmail.com>
2021-06-01 20:53:10 +0530

    initial commit

---
+++ src/Components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae
+        quaerat fugit, cum provident quos amet reprehenderit dolorum in earum
+        ratione aperiam quae dignissimos, accusantium, aspernatur labore
+        voluptates recusandae consectetur repellendus unde. Eveniet molestiae
+        veritatis qui accusantium.
+      </p>
+    </div>
+  );
+};
